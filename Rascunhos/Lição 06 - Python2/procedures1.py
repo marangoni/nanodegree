6491e919b27642fdfa7bd291eaa2bf15fdcfc283
Luiz Marangoni <oliveiralcm@gmail.com>
2017-12-30 20:54:11 -0200

    Python ok

---
+++ Rascunhos/Lição 06 - Python2/procedures1.py
@@ -0,0 +1,22 @@
+def rest_of_string(s):
+    return s[1:]
+
+print rest_of_string('Audacity')
+
+def square(x):
+    return x*x
+
+print square(5)
+print square(25)
+
+def sum3(a,b,c):
+    return a + b + c
+
+print sum3(1,2,3)
+print sum3(-1,2,-3)
+
+def abbaize(a,b):
+    return a + b + b + a
+
+print abbaize('a','b')
+print abbaize('cat','dog')
---
+++ Rascunhos/Lição 07 - Python2/procedures2.py
@@ -0,0 +1,24 @@
+def udacify(s):
+    return 'U' + s
+
+print udacify('eunacians')
+print udacify('paLele')
+
+def double1(x):
+    return 2 * x
+
+def double2(x):
+    print 2 * x
+
+def double3(x):
+    return 2 * x
+    print 2 * x
+
+def double4(x):
+    print 2 * x
+    return 2 * x
+
+double1(1)
+double2(2)
+double3(3)
+double4(4)
